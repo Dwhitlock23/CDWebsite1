17eec3c76db3b805b2fe09b5a85f75ddf80d596a
Dwhitlock23 <72172232+Dwhitlock23@users.noreply.github.com>
2020-11-02 19:50:39 -0500

    Revert to old mysqli method

--- viewer-service.php
+++ viewer-service.php
@@ -1,32 +1,15 @@
-<?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:davis-server1.database.windows.net,1433; Database = Grades", "Dwhitlock23", "tihw_sivad23");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "Dwhitlock23", "pwd" => "{tihw_sivad23}", "Database" => "Grades", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:davis-server1.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-?>
-
 
 <?php
 
-// $cloudhost = "...azure.net";
-// $username = "un";
-// $password = "pw";
-// $database = "gradesDB"; #db I want to connect to. Schema for grades db
+$cloudhost = "davis-server1.database.windows.net";
+$username = "Dwhitlock23";
+$password = "tihw_sivad23";
+$database = "Grades"; #db I want to connect to. Schema for grades db
 
-// $conn = new mysqli($cloudhost, $username, $password, $database)
-// if ($conn -> connect_errno) {
-//     die("Failed to connect to MySQL: " . $conn -> connect_error);
-// }
+$conn = new mysqli($cloudhost, $username, $password, $database)
+if ($conn -> connect_errno) {
+    die("Failed to connect to MySQL: " . $conn -> connect_error);
+}
 
 //Get Grades
 if ($result = $conn -> query("SELECT studentID, grades FROM gradesTable")){
@@ -36,7 +19,7 @@ if ($result = $conn -> query("SELECT studentID, grades FROM gradesTable")){
         while($row = $result-> fetch_assoc()) {
             echo "<tr>";
             echo "<td>" . $row["studentID"] . "</td>";
-            echo "<td>" . $row["grades"] . "</td>";
+            echo "<td>" . $row["grade"] . "</td>";
             echo "</tr>";
         }
     ?>
