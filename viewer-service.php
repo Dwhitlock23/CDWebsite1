5d4fad50ea83ac5d6b7f643836fe351b4cfcc668
Dwhitlock23 <72172232+Dwhitlock23@users.noreply.github.com>
2020-11-02 20:05:49 -0500

    update connection method

--- viewer-service.php
+++ viewer-service.php
@@ -1,15 +1,19 @@
-
 <?php
-
-$cloudhost = "davis-server1.database.windows.net";
-$username = "Dwhitlock23";
-$password = "tihw_sivad23";
-$database = "Grades"; #db I want to connect to. Schema for grades db
-
-$conn = new mysqli($cloudhost, $username, $password, $database)
-if ($conn -> connect_errno) {
-    die("Failed to connect to MySQL: " . $conn -> connect_error);
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:davis-server1.database.windows.net,1433; Database = Grades", "Dwhitlock23", "{tihw_sivad23}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// // SQL Server Extension Sample Code:
+// $connectionInfo = array("UID" => "Dwhitlock23", "pwd" => "{tihw_sivad23}", "Database" => "Grades", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+// $serverName = "tcp:davis-server1.database.windows.net,1433";
+// $conn = sqlsrv_connect($serverName, $connectionInfo);
+
 
 //Get Grades
 if ($result = $conn -> query("SELECT studentID, grades FROM gradesTable")){
