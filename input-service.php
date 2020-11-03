131b513760dfe42714d412ba3a8102a0f244ec5a
Dwhitlock23 <72172232+Dwhitlock23@users.noreply.github.com>
2020-11-02 19:25:56 -0500

    Update input-service.php

--- input-service.php
+++ input-service.php
@@ -1,14 +1,33 @@
 <?php
 
-$cloudhost = "...azure.net";
-$username = "un";
-$password = "pw";
-$database = "gradesDB"; #db I want to connect to. Schema for grades db
 
-$conn = new mysqli($cloudhost, $username, $password, $database)
-if ($conn -> connect_errno) {
-    die("Failed to connect to MySQL: " . $conn -> connect_error);
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:davis-server1.database.windows.net,1433; Database = Grades", "Dwhitlock23", "tihw_sivad23");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "Dwhitlock23", "pwd" => "{tihw_sivad23}", "Database" => "Grades", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:davis-server1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
+
+
+// $cloudhost = "...azure.net";
+// $username = "un";
+// $password = "pw";
+// $database = "gradesDB"; #db I want to connect to. Schema for grades db
+
+// $conn = new mysqli($cloudhost, $username, $password, $database)
+// if ($conn -> connect_errno) {
+//     die("Failed to connect to MySQL: " . $conn -> connect_error);
+// }
 
 $studentID = $_POST("studentID");
 $grades = $_POST("grades");
@@ -16,4 +35,5 @@ $grades = $_POST("grades");
 $stmt = $conn->prepare("INSERT INTO gradesTable (studentID, grades) VALUES (?, ?)");
 $stmt->bind_param("di",$studentID, $grades);
 
+$stmt->execute();
 ?>
