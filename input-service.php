22b31b13f5c96efaac301075f8af2186d6f0943c
Dwhitlock23 <72172232+Dwhitlock23@users.noreply.github.com>
2020-11-05 09:59:45 -0500

    corrected table names

--- input-service.php
+++ input-service.php
@@ -1,18 +1,18 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:davis-server1.database.windows.net,1433; Database = Grades", "Dwhitlock23", "{tihw_sivad23}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+$cloudhost = "davis-server1.database.windows.net";
+$username = "Dwhitlock23";
+$password = "tihw_sivad23";
+$database = "Grades"; #db I want to connect to. Schema for grades db
+
+$conn = new mysqli($cloudhost, $username, $password, $database)
+if ($conn -> connect_errno) {
+    die("Failed to connect to MySQL: " . $conn -> connect_error);
 }
 
 $studentID = $_POST("studentID");
 $grades = $_POST("grades");
 
-$stmt = $conn->prepare("INSERT INTO gradesTable (studentID, grade) VALUES (?, ?)");
+$stmt = $conn->prepare("INSERT INTO grades (studentID, grade) VALUES (?, ?)");
 $stmt->bind_param("id",$studentID, $grades);
 
 $stmt->execute();
