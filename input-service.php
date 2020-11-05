9bb1f9bfb300ef3da855bd9f811c5ded9a564f0d
Dwhitlock23 <72172232+Dwhitlock23@users.noreply.github.com>
2020-11-05 10:09:28 -0500

    Update input-service.php

--- input-service.php
+++ input-service.php
@@ -1,19 +1,17 @@
 <?php
-$cloudhost = "davis-server1.database.windows.net";
-$username = "Dwhitlock23";
-$password = "tihw_sivad23";
-$database = "Grades"; #db I want to connect to. Schema for grades db
-
-$conn = new mysqli($cloudhost, $username, $password, $database)
-if ($conn -> connect_errno) {
-    die("Failed to connect to MySQL: " . $conn -> connect_error);
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:davis-server1.database.windows.net,1433; Database = Grades", "Dwhitlock23", "{tihw_sivad2}");
+    $conn = new PDO("sqlsrv:server = tcp:davis-server1.database.windows.net,1433; Database = Grades", "Dwhitlock23", "{tihw_sivad23}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 $studentID = $_POST("studentID");
 $grades = $_POST("grades");
-
 $stmt = $conn->prepare("INSERT INTO grades (studentID, grade) VALUES (?, ?)");
 $stmt->bind_param("id",$studentID, $grades);
-
 $stmt->execute();
 ?>
