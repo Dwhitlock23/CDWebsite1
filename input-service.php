b62156c5e3ff1f963f2eba38bebc9440f214acd5
Dwhitlock23 <72172232+Dwhitlock23@users.noreply.github.com>
2020-11-08 15:17:59 -0500

    hello world

--- input-service.php
+++ input-service.php
@@ -1,18 +1,18 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:davis-server1.database.windows.net,1433; Database = Grades", "Dwhitlock23", "{tihw_sivad23}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+// try {
+//     $conn = new PDO("sqlsrv:server = tcp:davis-server1.database.windows.net,1433; Database = Grades", "Dwhitlock23", "{tihw_sivad23}");
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// }
+// catch (PDOException $e) {
+//     print("Error connecting to SQL Server.");
+//     die(print_r($e));
+// }
 
-$studentID = $_POST("studentID");
-$grades = $_POST("grades");
+// $studentID = $_POST("studentID");
+// $grades = $_POST("grades");
 
-$sql = "INSERT INTO grades (studentID, grade) VALUES (?, ?)";
-$stmt->execute([$studentID, $grades]);
-
+// $sql = "INSERT INTO grades (studentID, grade) VALUES (?, ?)";
+// $stmt->execute([$studentID, $grades]);
+echo("Hello World")
 ?>
