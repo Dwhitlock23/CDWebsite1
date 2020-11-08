52982fdb04276a74d50b893e072701ca13aeda82
Dwhitlock23 <72172232+Dwhitlock23@users.noreply.github.com>
2020-11-08 15:20:10 -0500

    Update input-service.php

--- input-service.php
+++ input-service.php
@@ -1,16 +1,16 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
-// try {
-//     $conn = new PDO("sqlsrv:server = tcp:davis-server1.database.windows.net,1433; Database = Grades", "Dwhitlock23", "{tihw_sivad23}");
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// }
-// catch (PDOException $e) {
-//     print("Error connecting to SQL Server.");
-//     die(print_r($e));
-// }
+try {
+    $conn = new PDO("sqlsrv:server = tcp:davis-server1.database.windows.net,1433; Database = Grades", "Dwhitlock23", "{tihw_sivad23}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-// $studentID = $_POST("studentID");
-// $grades = $_POST("grades");
+$studentID = $_POST("studentID");
+$grades = $_POST("grades");
 
 // $sql = "INSERT INTO grades (studentID, grade) VALUES (?, ?)";
 // $stmt->execute([$studentID, $grades]);
