044cc3eac05bfd4ecc2befd3b501eb57f8da77f5
Dwhitlock23 <72172232+Dwhitlock23@users.noreply.github.com>
2020-11-08 22:10:15 -0500

    Update input-service.php

--- input-service.php
+++ input-service.php
@@ -19,7 +19,7 @@ echo ("Grade: ");
 // $stmt->execute([$name, $surname, $sex]);
 echo("Adding grade 21 for id 68")
 $sql = "INSERT INTO grades (studentID, grade) VALUES (?, ?)";
-$stmt = $conn->prepare($sql);
+// $stmt = $conn->prepare($sql);
 // $stmt->execute([68, 21]);
 // $stmt = $link->prepare('INSERT INTO testtable (name, lastname, age)
 //     VALUES (:fname, :sname, :age)');
