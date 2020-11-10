be0c0bb01526ed000efa346b634b59156257f899
Dwhitlock23 <72172232+Dwhitlock23@users.noreply.github.com>
2020-11-09 21:32:29 -0500

    Adding basic auth logic

--- input-service.php
+++ input-service.php
@@ -9,31 +9,41 @@ catch (PDOException $e) {
     die(print_r($e));
 }
 
+//     && $_SERVER['PHP_AUTH_USER'] == 'myuser' 
+//     && $_SERVER['PHP_AUTH_PW'] == 'mypswd'))
 
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // collect value of input fields
-    $studentID = $_POST['studentID'];
-    $grades = $_POST['grades'];
-    // debug messages:
-    if (empty($studentID)) {
-        echo "studentID is empty";
-    } 
-    else {
-        echo $studentID;
-    }
-    if (empty($grades)) {
-        echo "grades is empty";
-    } 
-    else {
-        echo $grades;
-    }
+if (!(isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) 
+{
+    header('WWW-Authenticate: Basic realm="Restricted area"');
+    header('HTTP/1.1 401 Unauthorized');
+    exit;
 }
+else{
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      // collect value of input fields
+        $studentID = $_POST['studentID'];
+        $grades = $_POST['grades'];
+        // debug messages:
+        if (empty($studentID)) {
+            echo "studentID is empty";
+        } 
+        else {
+            echo $studentID;
+        }
+        if (empty($grades)) {
+            echo "grades is empty";
+        } 
+        else {
+            echo $grades;
+        }
+    }
 
 
 
-$sql = "INSERT INTO grades (studentID, grade) VALUES (?, ?)";
-$conn->prepare($sql)->execute([$studentID, $grades]);
-//close connection
-$conn -> close();
+    $sql = "INSERT INTO grades (studentID, grade) VALUES (?, ?)";
+    $conn->prepare($sql)->execute([$studentID, $grades]);
+    //close connection
+    $conn -> close();
+}
 ?>
